74057e9e8f4d38cc7bba54760988046f8fd36e20
Joseph Cava <bink.lynch@gmail.com>
2023-09-17 21:36:35 -0600

    Some clean up.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,11 @@
 package com.example.demo;
 
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatusCode;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -25,12 +23,14 @@ import jakarta.servlet.http.HttpServletRequest;
 @RestController
 public class DemoApplication {
 
+  // This would be in a database.
   private Map<String, Map<String, Object>> products = getProducts();
 
   public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
   }
 
+  // This would be in a database.
   private Map<String, Map<String, Object>> getProducts() {
     Map<String, Map<String, Object>> products = new HashMap<>() {
       {
@@ -51,37 +51,42 @@ public class DemoApplication {
     return products;
   }
 
-  @GetMapping("/charge")
-  public Map<String, Object> getCharge(@RequestBody Map<String, Object> body, HttpServletRequest request) {
-    Map<String, Object> map = new LinkedHashMap<>();
-    map.put("message", "hello");
-    return map;
-  }
-
   @PostMapping("/charge")
   @ResponseBody
   public String postCharge(@RequestBody Map<String, Object> body, HttpServletRequest request)
       throws StripeException {
+    // Get the secret key from your stripe account.
     Stripe.apiKey = "sk_test_ZJCeWvsgPVxby9ScCVDon78M";
 
+    // This is the token retrieved from stripe on the browser side.
     @SuppressWarnings("unchecked")
     final Map<String, Object> token = (Map<String, Object>) body.get("token");
+
+    // This is any additional data we send from the browser.
     @SuppressWarnings("unchecked")
     final Map<String, Object> data = (Map<String, Object>) body.get("data");
 
+    // Get the product by id from the database.
     String productId = (String) data.get("product");
     Map<String, Object> product = products.get(productId);
 
+    // If the product is not found, return an error.
     if (product == null) {
       throw new ResponseStatusException(HttpStatusCode.valueOf(400), "Product not found");
     }
 
+    // Add the product to data to be stored as metadata.
+    data.putAll(product);
+
+    // Add the token and product information to the charge parameters.
     Map<String, Object> params = new HashMap<>();
     params.put("source", token.get("id"));
     params.put("amount", product.get("amount"));
     params.put("currency", product.get("currency"));
     params.put("description", product.get("description"));
+    params.put("metadata", data);
 
+    // Create the charge.
     Charge charge = Charge.create(params);
     return new GsonBuilder().setPrettyPrinting().create().toJson(charge);
   }
